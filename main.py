bbe6b3c6fb77b61bef4c551988bb1b2448624818
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 02:53:49 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:50:34.094Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-05T23:53:48.039Z
+Commit: Resolve null pointer exception in user service
