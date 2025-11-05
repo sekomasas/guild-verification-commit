2af029d17523ecb19db0b2bc606dcf5d6a226f3e
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 02:49:49 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:27:28.738Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-05T23:49:47.560Z
+Commit: Add multi-language support
