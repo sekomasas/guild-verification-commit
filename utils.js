8dcdb91e4294fdbcceeab2dffe049c0e6d993e6e
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 02:54:58 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:54:36.724Z
-Commit: Fix database connection timeout
+Timestamp: 2025-11-05T23:54:56.908Z
+Commit: Resolve null pointer exception in user service
