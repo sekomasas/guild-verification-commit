972bb3f2ff22b9eccc709a028acaffefa8547ab6
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 02:50:05 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:49:48.988Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-05T23:50:03.665Z
+Commit: Fix broken links in documentation
