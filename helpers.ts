47b22561021101c35199f7d7c934c5e98b3ae616
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 02:50:54 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:50:44.865Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-05T23:50:52.654Z
+Commit: Fix CSS styling bug on mobile devices
