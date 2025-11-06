7222d4bd3d2cc4a3357e5e1add087ea2f285109e
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 03:01:54 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:01:39.155Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T00:01:53.172Z
+Commit: Fix CSS styling bug on mobile devices
