650bda3b627ab28c482e27376382d3c80de74d99
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 03:03:06 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:02:58.322Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:03:05.210Z
+Commit: Fix CSS styling bug on mobile devices
