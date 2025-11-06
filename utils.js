d69e09b410778b6d802cd8b57baf38c380381b12
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 03:03:10 +0300

    Fix race condition in async operations

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:02:50.457Z
-Commit: Patch API endpoint error handling
+Timestamp: 2025-11-06T00:03:08.957Z
+Commit: Fix race condition in async operations
