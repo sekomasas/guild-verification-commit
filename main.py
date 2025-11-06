ded2073cc62d4357280d109afa2f82bc8c2fdc84
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 03:03:15 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:03:05.210Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T00:03:14.227Z
+Commit: Fix broken links in documentation
