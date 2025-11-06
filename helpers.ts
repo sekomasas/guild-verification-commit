f4bda6a12b269fa33ce859b1ef838ccdc2d87f47
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 03:00:04 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:59:18.019Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-06T00:00:03.309Z
+Commit: Fix CSS styling bug on mobile devices
