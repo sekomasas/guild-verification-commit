078cb65ea9e92d5806dad40a5532f2f48c2deac6
filiter <113396520+sekomasas@users.noreply.github.com>
2025-11-06 03:04:48 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:04:46.008Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:04:47.413Z
+Commit: Fix memory leak in event handlers
